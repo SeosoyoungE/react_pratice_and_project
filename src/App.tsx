45edfabd1d7384a225accc1ce6d60f46bc86a8f0
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-11-28 05:03:43 +0000

    Vite + React + TS project create

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState<number>(0);
 
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="App">
+      <h1>Vite + React + TS</h1>
+    </div>
+  );
 }
 
 export default App;

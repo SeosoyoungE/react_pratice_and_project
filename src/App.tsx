ca5b401013fb8203acae36163d2afa5fe0204531
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-12-05 00:59:46 +0000

    add module.css

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <Post author="소영" body="멋있다" />
       <Post author="나영" body="바보같다" />
       <Post author="희정" body="착하다" />
-      <Post />
+      <Post author="소영" body="ㅋㅋ바보들~~" />
     </div>
   );
 }

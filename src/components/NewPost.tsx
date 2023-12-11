be8c5077d776e4c8fe9658a1bdca396c122d08c3
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-12-11 06:41:35 +0000

    submit 처리

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,14 +1,35 @@
 import classes from "./NewPost.module.css";
+import { useState } from "react";
 function NewPost({ onBodyChange, onNameChange, onCancel }) {
+  const [enteredBody, setEnterdBody] = useState("");
+  const [enteredName, setEnterdName] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnterdBody(event.target.value);
+  }
+  function nameChangeHandler(event) {
+    setEnterdName(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const post = {
+      body: enteredBody,
+      author: enteredName,
+    };
+    console.log(post);
+    onCancel();
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onBodyChange} />
+        <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={onNameChange} />
+        <input type="text" id="name" required onChange={nameChangeHandler} />
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={onCancel}>
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -5,29 +5,15 @@ import Modal from "./Modal";
 import { useState } from "react";
 
 function PostList({ modalIsVisible, hideModalHandler }) {
-  const [enteredBody, setEnterdBody] = useState("");
-  const [enteredName, setEnterdName] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnterdBody(event.target.value);
-  }
-  function nameChangeHandler(event) {
-    setEnterdName(event.target.value);
-  }
   return (
     <>
       {modalIsVisible ? (
         <Modal onClose={hideModalHandler}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onNameChange={nameChangeHandler}
-            onCancel={hideModalHandler}
-          />
+          <NewPost onCancel={hideModalHandler} />
         </Modal>
       ) : null}
 
       <ul className={styles.posts}>
-        <Post author={enteredName} body={enteredBody} />
         <Post author="나영" body="바보같다" />
         <Post author="희정" body="착하다" />
         <Post author="소영" body="바보들이당" />
